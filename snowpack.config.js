8ae2bbadb227f2053a2d7956e238ef466f8188f8
SU Hakim <bmsuhakim@gmail.com>
2021-03-04 12:23:01 +0600

    alhamdulillah,i am success

---
+++ snowpack.config.js
@@ -0,0 +1,11 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+    plugins: [
+        /* ... */
+        '@snowpack/plugin-sass', '@snowpack/plugin-babel',
+    ],
+    "mount": {
+        "src": "/dist",
+        "public": "/"
+    },
+};
---
+++ src/index.js
@@ -0,0 +1,4 @@
+function add() {
+    return 4 + 5
+}
+console.log(add());
